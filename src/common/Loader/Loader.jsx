0b4ae960149d02490869729fbcf5104e293c064a
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-04-25 22:58:28 +0200

    feature(loader): add Loader component

---
+++ src/common/Loader/Loader.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useTheme } from 'styled-components';
+import { RotatingLines } from 'react-loader-spinner';
+
+import { LoaderWrap } from './Loader.styled';
+
+const Loader = ({ className, width = '100' }) => {
+	const theme = useTheme();
+
+	return (
+		<LoaderWrap className={className}>
+			<RotatingLines
+				strokeColor={theme.colors.main}
+				strokeWidth='5'
+				animationDuration='0.75'
+				width={width}
+				visible={true}
+			/>
+		</LoaderWrap>
+	);
+};
+
+export default Loader;
+
+Loader.propTypes = {
+	width: PropTypes.string,
+	className: PropTypes.string,
+};
